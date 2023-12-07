9562146827ad9e023c19d9c607e12cb986073ae0
Sohail Mumtaz <sohailmumtaz749@gmail.com>
2023-12-07 15:24:40 +0500

    Push SimonSay

--- SimonSay/script.js
+++ SimonSay/script.js
@@ -15,6 +15,7 @@ document.addEventListener("keypress", function(){
 });
 
 function levelup(){
+    userseq=[];
     level++;
     result.innerText=`Level ${level}`;
 
@@ -22,6 +23,7 @@ function levelup(){
     let randColor=btns[randIndx];
     let randbtn=document.querySelector(`.${randColor}`);
     gameseq.push(randColor);
+    console.log(gameseq);
     flashbtn(randbtn);
 }
 
@@ -32,14 +34,45 @@ function flashbtn(btn){
     },200);
 }
 
+function checkAns(idx){
+    if(gameseq[idx]===userseq[idx]){
+        if(gameseq.length == userseq.length){
+            setTimeout(levelup(), 1000);
+        }
+    }
+    else 
+        {
+            result.innerText=`Game Over! Achieved level: ${level}`;
+            document.querySelector("body").style.backgroundColor = "red"
+            setTimeout(function(){
+                document.querySelector("body").style.backgroundColor="white";
+            },1000)
+            reset();
+        }
+}
 function btnPress(){
     flashbtn(this);
+
+    let userColor=this.getAttribute("id");
+    console.log(userColor);
+    userseq.push(userColor);
+    console.log("game sequence", gameseq);
+    console.log("User Sequence",userseq);
+
+    checkAns(userseq.length-1);
 }
 
 let allBtns=document.querySelectorAll(".btn");
 
 for (btn of allBtns){
     btn.addEventListener("click", btnPress);
+}
+
+function reset(){
+    start = false;
+    gameseq=[]
+    userseq=[]
+    level=0;
 }
 
 

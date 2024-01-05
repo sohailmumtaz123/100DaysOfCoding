6f786bb1fd144640cb6c10fbe20ea6b469a31d69
Sohail Mumtaz <sohailmumtaz749@gmail.com>
2024-01-05 12:07:55 +0500

    Learned about Sending Response, Routing, Nodemon

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -7,6 +7,21 @@ app.listen(port, ()=> {
     console.log("App is listening");
 })
 
-app.use((req, res)=> {
-    console.log("Request received")
+// app.use((req, res)=> {
+//     console.log("Request received")
+//     let output="<h1>Hi Sohail! This is response from server</h1> <h3> Here is the list of my Skills.</h3> <ul><li>Javascript</li> <li>React.js</li></ul>";
+//     res.send(output);
+// });
+
+// ------routing-----
+app.get("/", (req, res)=>{
+    res.send("<h1>You are connected with root path Sohail! Nodemon is active now.</h1>");
+});
+
+app.get("/about", (req, res)=> {
+    res.send("<h2>You are connected with about path. Let's check it with nodemon.</h2>");
+});
+
+app.get("*", (req, res)=>{
+    res.send("<h3>This is wild path response. Random Path </h3>");
 });

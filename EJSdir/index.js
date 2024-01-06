62aa424ffbe0e3beea11b6fe978e837c7099449c
Sohail Mumtaz <sohailmumtaz749@gmail.com>
2024-01-06 17:24:44 +0500

    Interpolation, passing data to EJS Template

--- EJSdir/index.js
+++ EJSdir/index.js
@@ -10,6 +10,15 @@ app.get("/", (req,res)=>{
     // res.send("This is Home.");
     res.render("home.ejs");
 });
+app.get("/rolldice", (req, res)=>{
+    let getfromDB=Math.floor(Math.random()*6)+1;//let suppose this value is coming from Databse
+    res.render("rolldice.ejs",{num:getfromDB}); //send the value to EJS
+})
+app.get("/ig/:username",(req, res)=>{
+    let {username}=req.params;
+    console.log(username);
+    res.render("instagram.ejs", {username});
+})
 
 app.get("/sohail", (req,res)=>{
     res.send("Hello Sohail. This is not EJS Template");
